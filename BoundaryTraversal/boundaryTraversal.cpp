b955e8dc9fd15eef1be2882c340537fbb4f4a9cb
Arihant Sai Paruchuru <Arihant1467@users.noreply.github.com>
2017-12-30 19:10:14 +0530

    BoundaryTraversal
    This program helps you traverse the tree from the boundary.
    STEPS :
    1. Traverse the left nodes and minus the leaf node in top to bottom manner
    2. Traverse the leaf nodes in left to right manner.
    3. Traverse the right nodes in bottom up manners

---
+++ BoundaryTraversal/boundaryTraversal.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+struct node{
+    int data;
+    node *left, *right;
+};
+node *getNewNode(int d){
+    node *n = new node;
+    n->left = NULL;n->right = NULL;n->data = d;
+    return n;
+}
+// To print Leaf
+void printLeaves(node *root){
+      if(root){
+        printLeaves(root->left);
+          if(root->left == NULL && root->right == NULL){
+            cout<<root->data<<"  ";
+          }
+        printLeaves(root->right);  
+      }
+}
+// To print Left boundary
+void printLeftBoundary(node *root){
+    if(root->left){
+      cout<<root->data<<"  ";
+      printLeftBoundary(root->left);
+    }
+}
+// To print right boundary
+void printRightBoundary(node *root){
+  if(root->right){
+    printRightBoundary(root->right);
+    cout<<root->data<<"  ";
+  }
+}
+void doBoundaryTraversal(node *root){
+  if(root){
+    cout<<root->data<<"  ";
+    printLeftBoundary(root->left);
+    printLeaves(root->left);
+    printLeaves(root->right);
+    printRightBoundary(root->right);
+  }
+}
+int main(){
+ // Tree Construction
+    node *root = getNewNode(20);
+    root->left = getNewNode(8);
+    root->right = getNewNode(22);
+    root->left->left = getNewNode(4);
+    root->left->right = getNewNode(12);
+    root->left->right->left = getNewNode(10);
+    root->left->right->right = getNewNode(14);
+    root->right->right = getNewNode(25);
+    doBoundaryTraversal(root);
+    return 0;
+}
