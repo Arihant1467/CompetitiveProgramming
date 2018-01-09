9d4164f07b62af6d54c60cd00fa2b6434d2fc943
Arihant1467 <arihant95@gmail.com>
2018-01-09 22:54:26 +0530

    chicks zoo question completed

--- chicksGeeks/chicksGeeks.cpp
+++ chicksGeeks/chicksGeeks.cpp
@@ -1,22 +1,28 @@
 #include<iostream>
 using namespace std;
-int arr[35+7] ={0};
-void preFetch(){
-    ::arr[0] = 1;
-    for(int i=0;i<35;++i){
-        int n = 2*::arr[i];
-        for (int j = i+1; j < i+1+6; ++j){
-            ::arr[j] = ::arr[j]+n;
+int main(){
+    int arr[35+7] ={0};
+    // arr[0] = 1;arr[1] = 1;arr[2] = 1;
+    // arr[3] = 1;arr[4] = 1;arr[5] = 1;
+    // for(int i=0;i<35;++i){
+    //         int n = 2*arr[i];
+    //         for (int j = i+1; j < i+1+6; ++j){
+    //             arr[j] = arr[j]+n;
+    //         }
+    // }
+    arr[0]=1;
+    for(int i=0;i<37;++i){
+        if(i-5>=0){
+            arr[i+1] = (arr[i]-arr[i-5])*3;
+        }else{
+            arr[i+1] =3*arr[i];
         }
     }
-}
-int main(){
-    preFetch();
     int test = 0; int query = 0;
     cin>>test;
     while(test--){
         cin>>query;
-        cout<<::arr[query-1]<<endl;
+        cout<<arr[query-1]<<endl;
     }
     return 0;
 }
