a7a39bd29a350dda75b5093189adcfae18e287fd
Arihant1467 <arihant95@gmail.com>
2019-05-11 17:43:43 -0700

    video stitching readme updated

--- VideoStiching/README.md
+++ VideoStiching/README.md
@@ -13,6 +13,21 @@ We have to find the minimum path sum by choosing each element from every row and
 
 
 #### Algorithm
+1. Create a class called `TimePoint` which will have start and end time values of each element in array
+
+2. We create an array of objects of `TimePoint` called `timestamps`.
+
+3. We will sort this array based on `start` point
+
+4. For each object in the array we will try to gather the intervals in our list based on different conditions. The conditions are as follows:
+
+- If the interval is covering a point not covered by the result list, add it to the result.
+- If its leaving a gap, then we cant fill that gap, hence return -1
+- If its overlapping, based on end time,
+    - We can either discard this one, or
+    - Include this one and replace the last interval of the result or
+    - Append this one.
+
 
 
 
--- VideoStiching/solution.py
+++ VideoStiching/solution.py
@@ -1,4 +1,4 @@
-
+import collections
 class TimePoint:
     def __init__(self,start,end):
         self.start = start
@@ -27,46 +27,20 @@ class Solution:
         
         for i in range(1,len(timestamps)):
             time = timestamps[i]
-            
-            if result[-1].end>=T or result[-1].end<time.start:
+            if result[-1].end >= T or time.start > result[-1].end:
                 break
-            
-    
-            if result[-1].start==time.start and result[-1].end==time.end:
+            if time.start == result[-1].start and time.end > result[-1].end:
+                result[-1] = time
+            elif time.end <= result[-1].end:
                 continue
-            
-            elif result[-1].start>=time.start and result[-1].end<time.end:
-                result[-1]=time
-                continue
-            
-            elif result[-1].end>=time.end:
-                continue
-            
-            elif result[-1].start >= time.start:
-                result[-1]=TimePoint(result[-1].start,time.end)
-            
+            elif time.start <= result[-1].start:
+                result[-1] = TimePoint(result[-1].start, time.end)
             else:
                 result.append(TimePoint(result[-1].end, time.end))
         
         return len(result) if result[0].start==0 and result[-1].end>=T else -1
         
-        '''
-        timepoint = collections.namedtuple('timepoint', ['start', 'end'])
-        times = sorted([timepoint(clip[0], clip[1]) for i, clip in enumerate(clips)])
-        res = [times[0]]
-        for time in times:
-            if res[-1].end >= T or time.start > res[-1].end:
-                break
-            if time.start == res[-1].start and time.end > res[-1].end:
-                res[-1] = time
-            elif time.end <= res[-1].end:
-                continue
-            elif time.start <= res[-1].start:
-                res[-1] = timepoint(res[-1].start, time.end)
-            else:
-                res.append(timepoint(res[-1].end, time.end))
-        return len(res) if res[0].start == 0 and res[-1].end >= T else -1
-        '''
+        
                 
             
     
